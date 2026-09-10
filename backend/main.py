775d92d032928547c639f60f5706bae514ff5ad1
d-q222 <279808976+d-q222@users.noreply.github.com>
2026-09-09 22:30:00 -0400

    FastAPI Skeleton /people/id route

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from backend.database import get_db
 from backend.models import People
 from sqlalchemy.orm import Session
@@ -19,3 +19,10 @@ def get_people_count(db: Session = Depends(get_db)):
 def list_people(db: Session = Depends(get_db)):
     people = db.query(People).order_by(People.id).all()
     return people
+
+@app.get("/people/{person_id}", response_model = PersonOut)
+def get_person(person_id: int, db: Session = Depends(get_db)):
+    person = db.query(People).filter(People.id == person_id).first()
+    if person is None:
+            raise HTTPException(status_code = 404, detail = "Person not found")
+    return person
